67d61a29ca8b2e812529be0f856cb46bad26df4e
Saifur Rahman <saifurrshishir@gmail.com>
2022-11-30 08:48:33 +0600

    DooBee is done

---
+++ Chapter-01/DooBee.java
@@ -0,0 +1,17 @@
+public class DooBee 
+{
+    public static void main(String[] args)
+    {
+        int x = 1;
+        while(x < 3)
+        {
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x += 1; //on each iteration increase x by 1 
+        }
+        if(x == 3)
+        {
+            System.out.print("Do");
+        }
+    }
+}
--- Chapter-01/MyFirstApp.java
+++ Chapter-01/MyFirstApp.java
@@ -3,5 +3,6 @@ public class MyFirstApp
 	public static void main(String[] args)
 	{
 		System.out.println("I Rule!");
+		System.out.println("The World");
 	}
 }
