05d2ffd6ad6df46989e915f1e7cb29ab9bac961e
sifurr <saifurrshishir@gmail.com>
2022-12-02 16:10:51 +0600

    Chapter-04 complete

---
+++ Chapter-04/Foo.java
@@ -0,0 +1,10 @@
+class Foo
+{
+	public void go()
+	{
+		// int x;
+		int x = 0; 
+		int z = x + 3; // x can't be used without initialization, because local variable needs to initialize before any use
+	}
+}
+
