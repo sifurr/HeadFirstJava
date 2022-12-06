72a3d6ecee9977a557bd040da78d1c65eae082ea
sifurr <saifurrshishir@gmail.com>
2022-12-07 02:10:13 +0600

    TreeSet done

---
+++ Chapter-11/BookCompare.java
@@ -0,0 +1,39 @@
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class BookCompare implements Comparator<Book> {
+    public int compare(Book one, Book two) {
+        return one.title.compareTo(two.title);
+    }
+}
+
+class TestTreeComparator {
+    public void go() {
+        Book b1 = new Book("How Cats Work");
+        Book b2 = new Book("Remix your Body");
+        Book b3 = new Book("Finding Emo");
+        BookCompare bookCompare = new BookCompare();
+        Set<Book> tree = new TreeSet<>(bookCompare);
+        tree.add(b1);
+        tree.add(b2);
+        tree.add(b3);
+        System.out.println(tree);
+    }
+}
+
+class Book implements Comparable<Book> 
+{
+    String title;
+
+    public Book(String t) 
+    {
+        title = t;
+    }
+    
+    @Override
+    public int compareTo(Book other)
+    {
+        return title.compareTo(other.title);
+    }
+}
---
+++ Chapter-11/Jukebox10.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Jukebox10 
+{
+    public static void main(String[] args) 
+    {
+        new Jukebox10().go();
+    }    
+    
+    public void go()
+    {
+        List<SongV4> songList = MockSongs.getSongsV4();
+        System.out.println(songList);
+
+        songList.sort((one, two) -> one.getTitle().compareTo(two.getTitle()));
+        System.out.println(songList);
+
+        Set<SongV4> songSetA = new TreeSet<>(songList);
+        System.out.println(songSetA);
+
+        Set<SongV4> songSetB = new TreeSet<>((o1, o2) -> o1.getBpm() - o2.getBpm());
+        songSetB.addAll(songList);
+        System.out.println(songSetB);
+
+    }    
+}
+
